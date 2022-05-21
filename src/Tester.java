4c1a573a1b2ba21f098c6327005e6e5569a5fa99
Zaker <zaker.omargeel@gmail.com>
2022-05-21 15:02:07 +0200

    Changes by cmjus

--- src/Tester.java
+++ src/Tester.java
@@ -1,8 +1,12 @@
 
+import Controller.Position;
 import base.GameController;
 
 public class Tester {
     public static void main(String[] args) {
+
+        //System.out.println(new Position(1, 1).equals(new Position(1, 1)));
+
 
         GameController g = new GameController();
         g.startGame();
